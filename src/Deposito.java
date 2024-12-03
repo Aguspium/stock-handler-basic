f624190486577a6750994c8e3b77bd511b2b8657
Aguspium <jakseek.jakseek@gmail.com>
2024-12-03 19:00:24 -0300

    exceptions

--- src/Deposito.java
+++ src/Deposito.java
@@ -4,6 +4,8 @@ import Inventario.Lacteo;
 import Inventario.Producto;
 import Inventario.Utils.Utils;
 
+import java.util.InputMismatchException;
+
 public class Deposito {
     public static void listaProductos() {
         if (Utils.listaproductos.isEmpty()) {
@@ -22,27 +24,33 @@ public class Deposito {
             System.out.println("Lista de Productos Vacia ");
 
         } else {
-            listaProductos();
-            System.out.println("Selecciona el producto a eleminar: ");
-            int eleccion = Utils.sc.nextInt() - 1;
-            Producto producto = Utils.listaproductos.get(eleccion);
-            System.out.println("Confirmas eliminar a: " + producto.getNombre() + " Si/No");
-            String opcion = Utils.sc.next();
-            if (opcion.equalsIgnoreCase("Si")) {
-                Utils.listaproductos.remove(eleccion);
-                System.out.println("Eliminacion completada");
-            } else {
-                System.out.println("Eliminacion cancelada");
+            try {
+                listaProductos();
+                System.out.println("Selecciona el producto a eleminar: ");
+                int eleccion = Utils.sc.nextInt() - 1;
+                Producto producto = Utils.listaproductos.get(eleccion);
+                System.out.println("Confirmas eliminar a: " + producto.getNombre() + " Si/No");
+                String opcion = Utils.sc.next();
+                if (opcion.equalsIgnoreCase("Si")) {
+                    Utils.listaproductos.remove(eleccion);
+                    System.out.println("Eliminacion completada");
+                } else {
+                    System.out.println("Eliminacion cancelada");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Opcion no valida");
+                Utils.sc.nextLine();
             }
         }
     }
 
-    public static void verProducto(){
-            if (Utils.listaproductos.isEmpty()) {
-                System.out.println("Lista de Productos Vacia ");
+    public static void verProducto() {
+        if (Utils.listaproductos.isEmpty()) {
+            System.out.println("Lista de Productos Vacia ");
 
-                } else {
+        } else {
 
+            try {
                 listaProductos();
                 System.out.println("elige el producto para ver: ");
                 int eleccion = Utils.sc.nextInt() - 1;
@@ -69,20 +77,30 @@ public class Deposito {
 
                     System.out.println("calcio: " + lacteo.getPorcentajecalcio() + "%");
 
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Opcion no valida");
+                Utils.sc.nextLine();
             }
         }
     }
 
-    public static void editarProducto(){
-    if(Utils.listaproductos.isEmpty()){
-        System.out.println("Lista de Productos Vacia ");
+    public static void editarProducto() {
+        if (Utils.listaproductos.isEmpty()) {
+            System.out.println("Lista de Productos Vacia ");
 
-        }else {
-        listaProductos();
-        System.out.println("Elige el producto a editar");
-        int eleccion = Utils.sc.nextInt() - 1;
+        } else {
+            try {
+                listaProductos();
+                System.out.println("Elige el producto a editar");
+                int eleccion = Utils.sc.nextInt() - 1;
 
-        Producto producto = Utils.listaproductos.get(eleccion);
-        producto.Editor(producto);}
+                Producto producto = Utils.listaproductos.get(eleccion);
+                producto.Editor(producto);
+            } catch (InputMismatchException e) {
+                System.out.println("Opcion no valida Solo numeros");
+                Utils.sc.nextLine();
+            }
+        }
     }
 }
--- src/Inventario/Utils/Creador.java
+++ src/Inventario/Utils/Creador.java
@@ -4,84 +4,99 @@ import Inventario.Fruta;
 import Inventario.Lacteo;
 import Inventario.Producto;
 import java.awt.*;
+import java.util.InputMismatchException;
 
 public class Creador {
 
-    public Creador(){
+    public Creador() {
+        try {
             System.out.println("Ingresa el tipo de producto: ");
             System.out.println("1) carne ");
             System.out.println("2) Fruta ");
             System.out.println("3) Lacteo ");
             int tipo = Utils.sc.nextInt();
-            agregarProducto(tipo);
+            if (tipo == 1 || tipo == 2 || tipo == 3) {
+                agregarProducto(tipo);
+            } else {
+                System.out.println("Opcion no valida");
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Opcion no valida");
+            Utils.sc.nextLine();
+        }
     }
 
-    public void agregarProducto(int tipo){
+    public void agregarProducto(int tipo) {
         String porsentaje;
         int cantidadGramos;
+        try {
+            System.out.println("Ingresa el nombre del producto");
+            String nombre = Utils.sc.next();
+            System.out.println("Ingresa la cantidad del producto");
+            int cantidad = Utils.sc.nextInt();
+            System.out.println("Ingresa el costo del producto");
+            double costo = Utils.sc.nextDouble();
 
-        System.out.println("Ingresa el nombre del producto");
-        String nombre = Utils.sc.next();
-        System.out.println("Ingresa la cantidad del producto");
-        int cantidad = Utils.sc.nextInt();
-        System.out.println("Ingresa el costo del producto");
-        double costo = Utils.sc.nextDouble();
+            switch (tipo) {
+                case 1: //Carnes
+                    System.out.println("Ingresa el % de grasa del producto del producto");
+                    porsentaje = Utils.sc.next();
+                    System.out.println("Ingresa la cantidad de proteinas del producto");
+                    cantidadGramos = Utils.sc.nextInt();
+                    Producto carnes = new Carnes(nombre, cantidad, costo, porsentaje, cantidadGramos);
+                    carnes.generadorPrecio();
+                    Producto.generateId();
+                    Utils.listaproductos.add(carnes);
 
-        switch (tipo){
-            case 1: //Carnes
-            System.out.println("Ingresa el % de grasa del producto del producto");
-                porsentaje = Utils.sc.next();
-            System.out.println("Ingresa la cantidad de proteinas del producto");
-                cantidadGramos = Utils.sc.nextInt();
-                Producto carnes = new Carnes(nombre, cantidad, costo, porsentaje, cantidadGramos);
-            carnes.generadorPrecio();
-            Producto.generateId();
-            Utils.listaproductos.add(carnes);
+                    break;
+                case 2: //Fruta
+                    System.out.println("Ingresa la cantidad de Gramos de azucar del producto");
+                    cantidadGramos = Utils.sc.nextInt();
+                    System.out.println("Ingresa la cantidad de agua del producto");
+                    porsentaje = Utils.sc.next();
+                    Producto fruta = new Fruta(nombre, cantidad, costo, porsentaje, cantidadGramos);
+                    fruta.generadorPrecio();
+                    Producto.generateId();
+                    Utils.listaproductos.add(fruta);
+                    new Menu();
 
-                break;
-            case 2: //Fruta
-                System.out.println("Ingresa la cantidad de Gramos de azucar del producto");
-                cantidadGramos = Utils.sc.nextInt();
-                System.out.println("Ingresa la cantidad de agua del producto");
-                porsentaje = Utils.sc.next();
-                Producto fruta = new Fruta(nombre, cantidad, costo, porsentaje, cantidadGramos);
-                fruta.generadorPrecio();
-                Producto.generateId();
-                Utils.listaproductos.add(fruta);
-                new Menu();
+                    break;
+                case 3: //Lacteos
+                    System.out.println("Ingresa el % de Agua del producto del Lacteo");
+                    porsentaje = Utils.sc.next();
+                    System.out.println("Ingresa el % de Calcio del producto");
+                    String porsentaje2 = Utils.sc.next();
+                    System.out.println("El producto es vegano? si/no");
+                    boolean vegano;
 
-                break;
-            case 3: //Lacteos
-                System.out.println("Ingresa el % de Agua del producto del Lacteo");
-                porsentaje = Utils.sc.next();
-                System.out.println("Ingresa el % de Calcio del producto");
-                String porsentaje2 = Utils.sc.next();
-                System.out.println("El producto es vegano? si/no");
-                boolean vegano;
+                    while (true) {
 
-                while (true) {
-
-                    String eleccion = Utils.sc.next();
-                    if (eleccion.equalsIgnoreCase("si")) {
-                        vegano = true;
-                        break;
-                    } else if (eleccion.equalsIgnoreCase("no")) {
-                        vegano = false;
-                        break;
-                    } else {
-                        System.out.println("Opcion erronea");
+                        String eleccion = Utils.sc.next();
+                        if (eleccion.equalsIgnoreCase("si")) {
+                            vegano = true;
+                            break;
+                        } else if (eleccion.equalsIgnoreCase("no")) {
+                            vegano = false;
+                            break;
+                        } else {
+                            System.out.println("Opcion erronea");
+                        }
                     }
-                }
 
-                Producto lacteo = new Lacteo(nombre, cantidad, costo, porsentaje, porsentaje2, vegano);
-                lacteo.generadorPrecio();
-                Producto.generateId();
+                    Producto lacteo = new Lacteo(nombre, cantidad, costo, porsentaje, porsentaje2, vegano);
+                    lacteo.generadorPrecio();
+                    Producto.generateId();
 
-                Utils.listaproductos.add(lacteo);
-                break;
-            default:
-                System.out.println("Opcion no valida");
+                    Utils.listaproductos.add(lacteo);
+                    break;
+                default:
+                    System.out.println("Opcion no valida");
 
+
+            }
+        }catch (InputMismatchException e) {
+            System.out.println("Opcion no valida");
+            Utils.sc.nextLine();
         }
     }
 }
