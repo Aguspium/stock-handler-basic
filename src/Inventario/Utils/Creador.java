f2ac9911e51f166cf695b2b595691f1866fa59c9
Aguspium <jakseek.jakseek@gmail.com>
2024-12-05 16:13:23 -0300

    Optimización Creador

--- src/Inventario/Utils/Creador.java
+++ src/Inventario/Utils/Creador.java
@@ -3,6 +3,7 @@ import Inventario.Carnes;
 import Inventario.Fruta;
 import Inventario.Lacteo;
 import Inventario.Producto;
+import Inventario.Validar.Validar;
 import java.util.InputMismatchException;
 
 public class Creador {
@@ -14,7 +15,7 @@ public class Creador {
             System.out.println("2) Fruta");
             System.out.println("3) Lácteo");
             System.out.println("4) Volver al menu");
-            int tipo = validarInt("");
+            int tipo = Validar.Int("");
 
             if (tipo >= 1 && tipo <= 3) {
                 agregarProducto(tipo);
@@ -22,23 +23,17 @@ public class Creador {
             } else if (tipo == 4) {
                 break;
             }else {
-                    System.out.println("Opción inválida.");
+                    System.err.println("Opción inválida.");
                 }
-
             }
         }
 
     public void agregarProducto(int tipo) {
         try {
-            System.out.println("Ingresa el nombre del producto:");
-            String nombre = Utils.sc.next();
-            System.out.println("Ingresa la cantidad del producto:");
-            int cantidad = Utils.sc.nextInt();
-            System.out.println("Ingresa el costo del producto:");
-            double costo = Utils.sc.nextDouble();
-
+            String nombre = Validar.Nombre();
+            int cantidad = Validar.Int("\nIngresa la cantidad del producto: ");
+            double costo = Validar.Double("\nIngresa el costo del producto");
             Producto producto = null;
-
             switch (tipo) {
                 case 1: // Carnes
                     producto = crearCarne(nombre, cantidad, costo);
@@ -50,64 +45,36 @@ public class Creador {
                     producto = crearLacteo(nombre, cantidad, costo);
                     break;
                 default:
-                    System.out.println("Opción no válida.");
+                    System.err.println("\nOpción no válida.");
             }
-
             if (producto != null) {
                 producto.generadorPrecio();
                 Producto.generateId();
                 Utils.listaproductos.add(producto);
-                System.out.println("Producto agregado exitosamente.");
+                System.out.println("\nProducto agregado exitosamente.");
             }
-
         } catch (InputMismatchException e) {
-            System.out.println("Dato ingresado no valido");
+            System.err.println("\nDato ingresado no valido");
             Utils.sc.nextLine();
         }
     }
 
     private Producto crearCarne(String nombre, int cantidad, double costo) {
-        int cantidadGrasa = validarInt("Ingresa el porcentaje de grasa: ");
-        int cantidadProteina = validarInt("Ingresa la cantidad de proteínas: ");
+        int cantidadGrasa = Validar.Int("\nIngresa el porcentaje de grasa: ");
+        int cantidadProteina = Validar.Int("\nIngresa la cantidad de proteínas: ");
         return new Carnes(nombre, cantidad, costo, cantidadGrasa, cantidadProteina);
     }
 
     private Producto crearFruta(String nombre, int cantidad, double costo) {
-        int cantidadAzucar = validarInt("Ingresa la cantidad de azúcar: ");
-        int cantidadAgua = validarInt("Ingresa el porcentaje de agua: ");
+        int cantidadAzucar = Validar.Int("\nIngresa la cantidad de azúcar: ");
+        int cantidadAgua = Validar.Int("\nIngresa el porcentaje de agua: ");
         return new Fruta(nombre, cantidad, costo, cantidadAgua, cantidadAzucar);
     }
 
     private Producto crearLacteo(String nombre, int cantidad, double costo) {
-        int cantidadAgua = validarInt("Ingresa el porcentaje de agua: ");
-        int cantidadCalcio = validarInt("Ingresa el porcentaje de calcio: ");
-        boolean vegano = validarOpcionSiNo("¿El lácteo es vegano? [Si/No]:");
+        int cantidadAgua = Validar.Int("\nIngresa el porcentaje de agua: ");
+        int cantidadCalcio = Validar.Int("\nIngresa el porcentaje de calcio: ");
+        boolean vegano = Validar.SiNo("\n¿El lácteo es vegano? [Si/No]:");
         return new Lacteo(nombre, cantidad, costo, cantidadAgua, cantidadCalcio, vegano);
-    }
-
-    private boolean validarOpcionSiNo(String mensaje) {
-        while (true) {
-            System.out.println(mensaje);
-            String eleccion = Utils.sc.next();
-            if (eleccion.equalsIgnoreCase("si")) {
-                return true;
-            } else if (eleccion.equalsIgnoreCase("no")) {
-                return false;
-            } else {
-                System.out.println("Opción errónea, ingresa [Si/No].");
-            }
-        }
-    }
-
-    private int validarInt(String mensaje) {
-        while (true) {
-            try {
-                System.out.println(mensaje);
-                return Utils.sc.nextInt();
-            } catch (InputMismatchException e) {
-                System.out.println("Error, ingresa un número.");
-                Utils.sc.nextLine();
-            }
-        }
     }
 }
