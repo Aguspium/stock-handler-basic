7de4cd01184289fe67ce1794985cc7658db34070
Aguspium <jakseek.jakseek@gmail.com>
2024-12-05 16:14:45 -0300

    Manejo de Validaciones

---
+++ src/Inventario/Validar/Validar.java
@@ -0,0 +1,64 @@
+package Inventario.Validar;
+import Inventario.Producto;
+import Inventario.Utils.Utils;
+import java.util.InputMismatchException;
+
+public class Validar {
+
+    public static int Int(String mensaje) {
+        while (true) {
+            try {
+                System.out.println(mensaje);
+                return Utils.sc.nextInt();
+            } catch (InputMismatchException e) {
+                System.err.println("\nError, ingresa un número.");
+                Utils.sc.nextLine();
+            }
+        }
+    }
+
+    public static double Double(String mensaje) {
+        while (true) {
+            try {
+                System.out.println(mensaje);
+                return Utils.sc.nextDouble();
+            } catch (InputMismatchException e) {
+                System.err.println("\nError, ingresa un número Ejemplo: [100,23].");
+                Utils.sc.nextLine();
+            }
+        }
+    }
+
+    public static boolean SiNo(String mensaje) {
+        while (true) {
+            System.out.println(mensaje);
+            String eleccion = Utils.sc.next();
+            if (eleccion.equalsIgnoreCase("si")) {
+                return true;
+            } else if (eleccion.equalsIgnoreCase("no")) {
+                return false;
+            } else {
+                System.err.println("\nOpción errónea, ingresa [Si/No].");
+            }
+        }
+    }
+
+    public static String Nombre() {
+        boolean a;
+        String nombre;
+        do {
+            System.out.println("\nIngresa el nombre del Producto: ");
+            nombre = Utils.sc.next();
+            a = true;
+            for (Producto p : Utils.listaproductos) {
+                if (nombre.equalsIgnoreCase(p.getNombre())) {
+                    System.err.println("\nEste producto ya existe prueba con otro!");
+                    a = false;
+                    break;
+                }
+            }
+        } while (!a);
+        System.out.println("\nNombre " + nombre + " Agregado con exito!");
+        return nombre;
+    }
+}
