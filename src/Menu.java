485d93ebae73ae86b8a8ffcda1c29ff2ee364461
Aguspium <jakseek.jakseek@gmail.com>
2024-12-05 16:39:30 -0300

    mejora de texto

--- src/Menu.java
+++ src/Menu.java
@@ -1,44 +1,34 @@
 import Inventario.Utils.Creador;
 import Inventario.Utils.Utils;
+import Inventario.Validar.Validar;
+
 import java.util.InputMismatchException;
 
 public class Menu {
 
     public Menu() {
-            while (true) {
-            try {
-                System.out.println("\nStock Handler");
-                System.out.println("1) Lista de Productos");
-                System.out.println("2) Añadir un Producto");
-                System.out.println("3) Eliminar un Producto");
-                System.out.println("4) Editar un Producto");
-                System.out.println("5) Cerrar el Programa \n");
+        while (true) {
+            System.out.println("""
+                --- Stock Handler ---
+                
+                1) Lista de Productos
+                2) Añadir un Producto
+                3) Eliminar un Producto
+                4) Editar un Producto
+                5) Cerrar el Programa
+                """);
+            int opcion = Validar.Int("Selecciona el Menu: ");
 
-                int opcion = Utils.sc.nextInt();
-
-                switch (opcion) {
-                    case 1:
-                        Deposito.verProducto();
-                        break;
-                    case 2:
-                        new Creador();
-                        break;
-                    case 3:
-                        Deposito.eliminarProducto();
-                        break;
-                    case 4:
-                        Deposito.editarProducto();
-                        break;
-                    case 5:
-                        System.out.println("Adios!");
-                        System.exit(0);
-                    default:
-                        System.out.println("Opción inválida");
-                        break;
+            switch (opcion) {
+                case 1 -> Deposito.verProducto();
+                case 2 -> new Creador();
+                case 3 -> Deposito.eliminarProducto();
+                case 4 -> Deposito.editarProducto();
+                case 5 -> {
+                    System.out.println("Adios!");
+                    System.exit(0);
                 }
-            } catch (InputMismatchException e) {
-                System.out.println("Opción inválida. Solo se permiten números, seleccione nuevamente.");
-                Utils.sc.nextLine();
+                default -> System.out.println("Opción inválida");
             }
         }
     }
